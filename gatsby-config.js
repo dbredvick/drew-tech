5f50dbdb569fe9ca31df0a72a353fa3617cdb6bc
Drew Bredvick <dbredvick@gmail.com>
2020-01-21 21:25:01 -0600

    task: add gtm

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,6 +43,21 @@ module.exports = {
       }
     },
     {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: "GTM-5RHXDKV",
+
+        // Include GTM in development.
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false,
+
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        // Defaults to null
+        defaultDataLayer: { platform: "gatsby" }
+      }
+    },
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `The Developer Spin`,
