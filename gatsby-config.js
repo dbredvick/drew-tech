3974bbf3605fae0e919842b7840c6cf99c832a88
Drew Bredvick <dbredvick@gmail.com>
2020-05-10 09:11:21 -0500

    task: fix fathom analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,8 +49,6 @@ module.exports = {
         // trackingUrl: 'your-fathom-instance.com',
         // Unique site id
         siteId: "BMQLUCRR",
-        // Domain whitelist
-        whitelistHostnames: ["drewb.tech"],
       },
     },
     {
